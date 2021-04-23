987760f42e0cab3434dccbc92bbaa7d4f4d5515e
crazy-oung <creativecloud@kakao.com>
2021-04-24 02:15:35 +0900

    init blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    siteTitleAlt: `Hackingrid`,
   },
   plugins: [
     {
@@ -25,12 +25,8 @@ module.exports = {
         ],
         externalLinks: [
           {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
-          },
-          {
-            name: `Instagram`,
-            url: `https://www.instagram.com/lekoarts.de/`,
+            name: `Github`,
+            url: `https://github.com/hackingrid`,
           },
         ],
       },
